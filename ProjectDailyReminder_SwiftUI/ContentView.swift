5bb5e6516aa30163f24e1fd6b19ad51f245c2029
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2019-10-24 09:19:55 +0300

    Initial Commit

---
+++ ProjectDailyReminder_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ProjectDailyReminder_SwiftUI
+//
+//  Created by Hoang Tran on 10/24/19.
+//  Copyright © 2019 Hoang Tran. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
