c0d17592de9b84c0301ccf6485abb5cc0a132578
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2019-11-25 14:48:49 +0200

    fix list

--- ProjectDailyReminder_SwiftUI/Dashboard.swift
+++ ProjectDailyReminder_SwiftUI/Dashboard.swift
@@ -9,59 +9,65 @@
 import SwiftUI
 
 struct Dashboard: View {
-    @State private var reminderData = [
-        "Go home",
-        "Do homework",
-        "Do something"
-    ]
+
+    let first = Reminder(name: "Item 1")
+    let second = Reminder(name: "Item 2")
+    let third = Reminder(name: "Item 3")
+    
     var body: some View {
-        NavigationView{
-            List{
-                HStack{
-                    ZStack{
-                        Rectangle()
-                            .fill(Color.green)
-                            .frame(width: 80, height: 80)
-                            .cornerRadius(10)
-                        Image(systemName: "plus")
-                            .resizable()
-                            .frame(width: 20, height: 20)
-                            .foregroundColor(Color.white)
-                    }
-                    Spacer()
-                    HStack{
-                        VStack(alignment: .leading){
+        UITableView.appearance().separatorColor = .clear
+        let reminders = [first, second, third]
+        return
+            NavigationView{
+                List{
+                    ForEach(reminders){reminder in
+                        HStack{
+                            ZStack{
+                                Rectangle()
+                                    .fill(Color.green)
+                                    .frame(width: 80, height: 80)
+                                    .cornerRadius(10)
+                                Image(systemName: "plus")
+                                    .resizable()
+                                    .frame(width: 20, height: 20)
+                                    .foregroundColor(Color.white)
+                            }
+                            Spacer()
                             HStack{
-                                Text("Reminder 0")
-                                Text("-")
-                                Text("Time")
+                                VStack(alignment: .leading){
+                                    HStack{
+                                        Text(reminder.name)
+                                        Text("-")
+                                        Text("Time")
+                                    }
+                                    .padding(5)
+                                    HStack(spacing: 20){
+                                        Text("M")
+                                            .frame(width: 30, height: 30)
+                                            .overlay(
+                                                RoundedRectangle(cornerRadius: 50)
+                                                    .stroke(Color.green, lineWidth: 5)
+                                        )
+                                        Text("T")
+                                        Text("W")
+                                        Text("T")
+                                        Text("F")
+                                        Text("S")
+                                        Text("S")
+                                            .foregroundColor(.red)
+                                    }
+                                }
+                                
                             }
-                            .padding(5)
-                            HStack(spacing: 20){
-                                Text("M")
-                                    .frame(width: 30, height: 30)
-                                    .overlay(
-                                        RoundedRectangle(cornerRadius: 50)
-                                            .stroke(Color.green, lineWidth: 5)
-                                )
-                                Text("T")
-                                Text("W")
-                                Text("T")
-                                Text("F")
-                                Text("S")
-                                Text("S")
-                                    .foregroundColor(.red)
-                            }
+                            Spacer()
                         }
-                        
+                        .padding(.trailing, 10)
+                        .background(Color.gray)
+                        .cornerRadius(10)
+                        .navigationBarTitle(Text("DateTime"), displayMode: .inline)
                     }
-                    Spacer()
                 }
-                .padding(.trailing, 10)
-                .background(Color.gray)
-                .cornerRadius(10)
-                
-            }
+               
         }
     }
 }
---
+++ ProjectDailyReminder_SwiftUI/Reminder.swift
@@ -0,0 +1,14 @@
+//
+//  Reminder.swift
+//  ProjectDailyReminder_SwiftUI
+//
+//  Created by Hoang Tran on 11/25/19.
+//  Copyright © 2019 Hoang Tran. All rights reserved.
+//
+
+import SwiftUI
+
+struct Reminder: Identifiable {
+    var id = UUID().uuidString
+    var name: String
+}
